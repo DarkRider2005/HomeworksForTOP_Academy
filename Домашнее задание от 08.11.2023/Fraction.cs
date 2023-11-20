4edd47aa86e4607c95426829c2707a101724dfc4
Georgiy <98572930+Dark16Developer22@users.noreply.github.com>
2023-11-20 17:44:58 +0500

    Add files via upload

---
+++ Домашнее задание от 08.11.2023/Fraction.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Program
+{
+    /*
+     Создайте класс Fraction для описания дроби. У неё должны быть
+свойства для числителя и знаменателя, открытые только для чтения. Добавьте
+проверку, что числитель никогда не должен становится равен 0. Напишите
+для этого класса перегрузку инкремента и декремента (изменяет числитель на
+числитель * знаменатель), оператора отрицания (меняет знак числителя), а
+также перегрузку true и false (класс равен false, если числитель равен 0).
+     */
+    public class Fraction
+    {
+        public int Numerator { get { return _numerator; } }
+        public int Denominator { get { return _denominator; } }
+        private int _numerator;
+        private int _denominator;
+
+        public Fraction(int numerator, int denominator)
+        {
+            _numerator = numerator;
+            if (_numerator == 0)
+                throw new ArgumentException("Числитель равен нулю", nameof(numerator));
+            
+            _denominator = denominator;
+        }
+
+        public static Fraction operator ++(Fraction frac)
+        {
+            return new Fraction(frac.Numerator * frac.Denominator, frac.Denominator);
+        }
+
+        public static Fraction operator --(Fraction frac)
+        {
+            return new Fraction(frac.Numerator * frac.Denominator, frac.Denominator);
+        }
+
+        public static Fraction operator -(Fraction frac)
+        {
+            return new Fraction(-frac._numerator, frac._denominator);
+        }
+
+        public static bool operator true(Fraction frac)
+        {
+            if (frac.Numerator != 0)
+                return true;
+            else return false;
+        }
+        public static bool operator false(Fraction frac)
+        {
+            if (frac.Numerator == 0)
+                return true;
+            else return false;
+        }
+    }
+}
