fbd8ce44b8337de265e3c05640bc896424e76fcf
Georgiy <98572930+Dark16Developer22@users.noreply.github.com>
2023-11-20 17:14:43 +0500

    Add files via upload

--- Домашнее задание от 03.11.2023/Program.cs
+++ Домашнее задание от 03.11.2023/Program.cs
@@ -2,28 +2,67 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
-using Program.TicTacToe;
+using Program.TaskOne;
+using Program.TaskTwo;
 
 namespace Program
-{ 
+{
     public class Program
     {
         static void Main(string[] args)
         {
-            Journal journal = new Journal();
-            TicTacToeGameManager ticTacToeGame = new TicTacToeGameManager();
-
             int action = 0;
-            Console.WriteLine("Для проверки желаемого задания введите его номер, для выхода - 3");
-            Console.Write("Я выбираю -> ");
-            action = Int32.Parse(Console.ReadLine());
+            Console.WriteLine("Для проверки желаемого задания напишите его номер, для выхода - 3");
+            try
+            {
+                action = int.Parse(Console.ReadLine());
+            }
+            catch(FormatException ex)
+            {
+                Console.WriteLine("Вводите целочисленные значения! ");
+                Thread.Sleep(1000);
+                Console.Clear();
+                Main(args);
+            }
 
-            if (action == 1)
-                ticTacToeGame.StartGame();
+            if(action == 1)
+            {
+                Builder builder = new Builder("Петя", "Пупкин", "Прораб", new DateTime(2000, 6, 11), 100000);
+                Sailor sailor = new Sailor("Кот", "Матроскин", "Капрал", new DateTime(1999, 2, 15), 145000);
+                Pilot pilot = new Pilot("Вася", "Мечтатель", "Курсант", new DateTime(1989, 6, 29), 200000);
+                Console.Write(builder.ToString() + "\n" + sailor.ToString() + "\n" + pilot.ToString());
+            }
+            else if(action == 2)
+            {
+                Teapot teapot = new Teapot("Я ЧАЙНИК", "ЛЮБЛЮ ХОЛОДНУЮ ВОДУ", 1.7f, new DateTime(2023, 11, 20));
+                Nuke nuke = new Nuke("Я МИКРОВОЛНОВКА", "ЛЮБЛЮ ГРЕТЬ ТАРЕЛКИ", "СВЧ + Гриль", new DateTime(2023, 11, 20));
+                Car car = new Car("Я АВТОМОБИЛЬ", "ЛЮБЛЮ ГОНЯТЬ ПО ГОРОДСКИМ УЛИЦАМ", 400f, new DateTime(2023, 11, 20));
+                Steamboat steamboat = new Steamboat("Я ПАРОХОД", "НРАВИТСЯ ПЛАВАТЬ", 5000f, new DateTime(2023, 11, 20));
 
-            else journal.SetData();
+                Console.Write(teapot.ToString());
+                teapot.Desk();
+                teapot.Sound();
+                Console.WriteLine();
 
+                Console.Write(nuke.ToString());
+                nuke.Desk();
+                nuke.Sound();
+                Console.WriteLine();
+
+                Console.Write(car.ToString());
+                car.Desk();
+                car.Sound();
+                Console.WriteLine();
+
+                Console.Write(steamboat.ToString());
+                steamboat.Desk();
+                steamboat.Sound();
+                Console.WriteLine();
+            }
+            else
+                return;
         }
     }
 }
---
+++ Домашнее задание от 03.11.2023/Teapot.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Program.TaskTwo
+{
+    public class Teapot : Device 
+    {
+        float _volume;
+        public Teapot(string title, string description, float volume ,DateTime dateOfCreation) : base(title, description, dateOfCreation) 
+        {
+            _volume = volume;
+        }
+
+        public override void Sound()
+        {
+            Console.WriteLine("*Издает звук* <Чых-Пых Я Кипячу Воду>");
+        }
+
+        public override string ToString()
+        {
+            return base.ToString() + $"Объем {_volume}л.\n";
+        }
+    }
+}
