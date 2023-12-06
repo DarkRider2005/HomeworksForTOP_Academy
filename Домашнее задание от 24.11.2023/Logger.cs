e42f6e95b64006f5a614f8aa38355e3953ef9209
Georgiy <98572930+Dark16Developer22@users.noreply.github.com>
2023-12-06 10:38:09 +0500

    Add files via upload

---
+++ Домашнее задание от 24.11.2023/Logger.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Program
+{
+    public class Logger
+    {
+        public static void AddLog(string text)
+        {
+            try
+            {
+                using (StreamWriter writer = new StreamWriter("Notes.txt", true))
+                {
+                    writer.WriteLine(DateTime.Today + " -> " +text);
+                    using (StreamWriter writer1 = new StreamWriter("Journal.log", true))
+                    {
+                        writer1.WriteLine(DateTime.Now + " Создана новая заметка");
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                using (StreamWriter writer1 = new StreamWriter("Journal.log", true))
+                {
+                    writer1.WriteLine(DateTime.Now + ex.Message);
+                }
+
+                return;
+            }
+        }
+
+        public static void ReadAllLog()
+        {
+            try
+            {
+                using (StreamReader reader = new StreamReader("Notes.txt", true))
+                {
+                    Console.WriteLine(" Данные полученные из файла <Notes.txt>");
+                    Console.WriteLine(reader.ReadToEnd());
+                    using (StreamWriter writer = new StreamWriter("Journal.log", true))
+                    {
+                        writer.WriteLine(DateTime.Now + " Заметки прочитаны");
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                using (StreamWriter writer = new StreamWriter("Journal.log", true))
+                {
+                    writer.WriteLine(DateTime.Now + ex.Message);
+                }
+
+                return;
+            }
+        }
+    }
+}
---
+++ Домашнее задание от 24.11.2023/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Program
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = " ";
+
+            Console.Write(" Введите текст заметки для записи в файл -> ");
+            text = Console.ReadLine();
+            
+            Logger.AddLog(text);
+            Logger.ReadAllLog();
+        }
+    }
+}
