c3c5679117b382da577fda2580b0a6f700dd4ef5
Georgiy <98572930+Dark16Developer22@users.noreply.github.com>
2023-12-06 10:17:37 +0500

    Add files via upload

---
+++ Домашнее задание от 27.11.2023/InvoicePayment.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Runtime.Serialization;
+
+/*
+ * Разработать класс «Счет для оплаты». В классе предусмотреть следующие
+поля:
+● оплата за день;
+● количество дней;
+● штраф за один день задержки оплаты;
+● количество дней задержки оплаты;
+● сумма к оплате без штрафа (вычисляемое поле);
+● штраф (вычисляемое поле);
+● общая сумма к оплате (вычисляемое поле).
+Вычисляемые поля не сериализуются. Разработать приложение, в котором
+необходимо продемонстрировать использование этого класса, результаты
+должны записываться и считываться из файла.
+ */
+
+// я не до конца понял смысл полей класса, поэтому сделал в меру своего понимания
+namespace Program
+{
+    [Serializable]
+    public class InvoicePayment
+    {
+        private int _numberOfDays;
+        private int _numberOfDaysOfPaymentDelay;
+
+        public int NumberOfDays { get { return _numberOfDays; } }
+        public int NumberOfDaysOfPaymentDelay { get { return _numberOfDaysOfPaymentDelay; } }
+
+        private double _paymentPerDay;
+        private double _penaltyForOneDayOfDelay;
+
+        [NonSerialized] private double _amountToBePaidWithoutPenalty;
+        [NonSerialized] private double _penalty;
+        [NonSerialized] private double _amount;
+
+        public double PaymentPerDay { get { return _paymentPerDay; } }
+        public double PenaltyForOneDayOfDelay { get { return _penaltyForOneDayOfDelay; } }
+
+        public InvoicePayment(int numberOfDays, int numberOfDaysOfPaymentDelay, double paymentPerDay, double penaltyForOneDayOfDelay)
+        {
+            _numberOfDays = numberOfDays;
+            _numberOfDaysOfPaymentDelay = numberOfDaysOfPaymentDelay;
+            _paymentPerDay = paymentPerDay;
+            _penaltyForOneDayOfDelay = penaltyForOneDayOfDelay;
+
+            Serialize(this);
+        }
+
+        public InvoicePayment() : this(1, 2, 1200d, 200d) { }
+
+        public static void Serialize(InvoicePayment invoice)
+        {
+            BinaryFormatter formatter = new BinaryFormatter();
+
+            using (FileStream fstream = new FileStream("Invoice.bin", FileMode.OpenOrCreate, FileAccess.Write))
+            {
+                formatter.Serialize(fstream, invoice);
+            }
+        }
+
+        public static InvoicePayment Deserialize(string path)
+        {
+            BinaryFormatter formatter = new BinaryFormatter();
+
+            try
+            {
+                using (FileStream fstream = new FileStream(path, FileMode.Open, FileAccess.Read))
+                {
+                    return (InvoicePayment)formatter.Deserialize(fstream);
+                }
+            }
+            catch (FileNotFoundException ex)
+            {
+                Console.WriteLine("Чтобы десериализовать данные, нужно сначала их сериализовать");
+                return null;
+            }
+        }
+
+        public double CalcAmountToBePaidWithoutPenalty(double[] prices)
+        {
+
+            for (int i = 0; i < prices.Length; i++)
+                _amountToBePaidWithoutPenalty += prices[i];
+
+            return _amountToBePaidWithoutPenalty;
+        }
+
+        public double CalcPenalty()
+        {
+            _penalty = _numberOfDaysOfPaymentDelay * _penaltyForOneDayOfDelay;
+
+            return _penalty;
+        }
+
+        public double CalcAmount()
+        {
+            _amount = _amountToBePaidWithoutPenalty + _penalty;
+
+            return _amount;
+        }
+    }
+}
---
+++ Домашнее задание от 27.11.2023/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Program
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            InvoicePayment invoice = null;
+            if (File.Exists("Invoice.bin"))
+                invoice = InvoicePayment.Deserialize("Invoice.bin");
+
+            else
+            {
+                invoice = new InvoicePayment(10, 5, 1200d, 200d); // создаю и сериализую(в конструкторе) объект класса
+                invoice = null;
+                invoice = InvoicePayment.Deserialize("Invoice.bin"); // десериализую созданный объект. Сделал так, потому что в задании написано "Информация должна браться из файла"
+            }
+
+            double[] prices = { 100d, 115.5d, 200.5d, 1200.5d };
+            Console.WriteLine($" Оплата за день: {invoice.PaymentPerDay} рублей");
+            Console.WriteLine($" Количество дней: {invoice.NumberOfDays}");
+            Console.WriteLine($" Штраф за один день задержки: {invoice.PenaltyForOneDayOfDelay} рублей");
+            Console.WriteLine($" Количество дней задержки оплаты: {invoice.NumberOfDaysOfPaymentDelay}");
+            Console.WriteLine($" Сумма к оплате без штрафа: {invoice.CalcAmountToBePaidWithoutPenalty(prices)} рублей");
+            Console.WriteLine($" Штраф: {invoice.CalcPenalty()} рублей");
+            Console.WriteLine($" Сумма к оплате: {invoice.CalcAmount()} рублей");
+
+        }
+    }
+}
