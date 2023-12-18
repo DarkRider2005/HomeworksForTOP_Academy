9952eed116e500eba8ddc22e6affd5081b5fc84f
Georgiy <98572930+Dark16Developer22@users.noreply.github.com>
2023-12-18 18:28:36 +0500

    Add files via upload

---
+++ Домашнее задание от 13.11.2023/ListBook.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+ Создайте приложение «Список книг для прочтения». Приложение
+должно содержать класс списка книг и позволять добавлять книги в список,
+удалять книги из списка, проверять есть ли книга в списке, и т. д. Реализуйте
+интерфейс IEnumerable и ICloneable для класса списка книг.
+ */
+namespace Program
+{
+    public class ListBook : IEnumerable, ICloneable
+    {
+        private List<string> _books = new List<string>();
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return _books.GetEnumerator();
+        }
+
+        object ICloneable.Clone()
+        {
+            return this;
+        }
+
+        public void Add(string item)
+        {
+            _books.Add(item);
+        }
+
+        public void Delete(int index)
+        {
+            _books.RemoveAt(index);
+        }
+
+        public bool Find(string item)
+        {
+            foreach(string book in _books)
+                if(item == book) return true;
+            
+            return false;
+        }
+    }
+}
