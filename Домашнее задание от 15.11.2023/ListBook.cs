4655f17358e2a05cef5625619e32eb9070955626
Georgiy <98572930+Dark16Developer22@users.noreply.github.com>
2023-12-18 18:50:44 +0500

    Add files via upload

---
+++ Домашнее задание от 15.11.2023/ListBook.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+ Создайте приложение «Список книг для прочтения». Приложение
+должно содержать класс списка книг и позволять добавлять книги в список,
+удалять книги из списка, проверять есть ли книга в списке, и т. д. Реализуйте
+интерфейс IEnumerable и ICloneable для класса списка книг.
+ */
+namespace Program
+{
+    public class ListBook : IEnumerable, ICloneable
+    {
+        private List<string> _books;
+
+        public ListBook()
+        {
+            _books = new List<string>();
+        }
+
+        public ListBook(string[] books) : this()
+        {
+            _books.AddRange(books);
+        }
+
+        public ListBook(List<string> books) : this()
+        {
+            _books = books;
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return _books.GetEnumerator();
+        }
+
+        object ICloneable.Clone()
+        {
+            return this;
+        }
+
+        public void Add(string item)
+        {
+            _books.Add(item);
+        }
+
+        public void Delete(int index)
+        {
+            try
+            {
+                if (index > _books.Count - 1 || index < 0)
+                    throw new ListBookException("Индекс вне диапазона доступных значений");
+
+                _books.RemoveAt(index);
+            }
+            catch (ListBookException ex)
+            {
+                Console.WriteLine(ex.Message);
+                return;
+            }
+        }
+
+        public void ShowBooks()
+        {
+            for (int i = 0; i < _books.Count; i++)
+                Console.WriteLine(i + ") книга: " + _books[i]);
+        }
+
+        public bool Find(string item)
+        {
+            foreach (string book in _books)
+                if (item == book) return true;
+
+            return false;
+        }
+    }
+}
---
+++ Домашнее задание от 15.11.2023/ListBookException.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Program
+{
+    public class ListBookException : SystemException
+    {
+        public ListBookException(string message) : base(message) { }
+    }
+}
