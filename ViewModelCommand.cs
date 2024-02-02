f68e7dc5e32ba311227c6938b3ed364b6098c19d
Georgiy <98572930+DarkRider2005@users.noreply.github.com>
2024-02-02 15:05:50 +0500

    Add files via upload

---
+++ ViewModelCommand.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPFProject
+{
+    public class ViewModelCommand : ModelCommand
+    {
+        private readonly Func<bool> _canExecute;
+        private readonly Action _execute;
+
+        public ViewModelCommand(Func<bool> canExecute, Action execute)
+        {
+            _canExecute = canExecute;
+            _execute = execute;
+        }
+
+        protected override bool CanExecute()
+        {
+            return _canExecute.Invoke();
+        }
+
+        protected override void Execute()
+        {
+            if (_execute != null)
+                _execute.Invoke();
+        }
+    }
+}
